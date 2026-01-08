d82bb631b56fcd79299a3954ed7ce58121a1f937
Callum Chalk <>
2026-01-08 17:46:24 +0000

    Disable unneeded RGB Matrix effects, and move as much of config.h as possible to info.json

--- keyboards/keychron/q1v1/ansi/keymaps/gozzle/config.h
+++ keyboards/keychron/q1v1/ansi/keymaps/gozzle/config.h
@@ -12,23 +12,13 @@
 
 #ifdef RGB_MATRIX_ENABLE
 
-  // turn off lights if suspended or after timeout
-#  define RGB_MATRIX_SLEEP
-#  define RGB_MATRIX_TIMEOUT 300000  // 5 min
-
-  // hard-coded colors for caps lock and fn layer
+// hard-coded colors for caps lock and fn layer
 #  define CAPS_LOCK_INDICATOR_COLOR_RGB RGB_RED
 #  define FN_LAYER_COLOR_RGB RGB_GREEN
 
 #  define RGB_MATRIX_KEYREACTIVE_ENABLED
 
-  // Only enable a few effects
-#  define ENABLE_RGB_MATRIX_SOLID_COLOR
-#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#  define ENABLE_RGB_MATRIX_SOLID_REACTIVE_BACKLIGHT
-
-// Leave as defaults
+// Leave as default
 // #  define RGB_MATRIX_SOLID_REACTIVE_BACKLIGHT_COLOR_HSV 0, 0, 96  // dim white
-// #  define RGB_MATRIX_KEYREACTIVE_DELAY 70
 
 #endif //RGB_MATRIX_ENABLE
